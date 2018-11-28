fdee50eebde665e3e651e61982345abc0bb85d37
nathias2004 <2015csb1037@iitrpr.ac.in>
2018-11-28 21:11:47 +0530

    Plots of answers taken in the survey

--- Experiment2/Graph_Loading.py
+++ Experiment2/Graph_Loading.py
@@ -2,6 +2,37 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
+Max_Hop = 5
+Max_credit = 2**(Max_Hop+1)
+
+
+def Caluculte_avg_distance_mobile(M1,M2,M3,M4):
+	for i in range(0,len(M1)):
+		sum1 = 0
+		sum2 = 0
+		sum3 = 0
+		sum4 = 0
+		for j in range(0,len(M1)):
+			if(i!=j):
+				p = len(nx.shortest_path(G,M1[i],M1[j]))
+				if p<=Max_Hop:
+					sum1 = sum1 + Max_credit*(1-(0.5**p))
+		for j in range(0,len(M2)):
+			p = len(nx.shortest_path(G,M1[i],M2[j]))
+			if p <=Max_Hop:
+				sum2 = sum2 + Max_credit*(1-(0.5**p))
+		for j in range(0,len(M3)):
+			p = len(nx.shortest_path(G,M1[i],M3[j]))
+			if p<=Max_Hop:
+				sum3 = sum3 + Max_credit*(1-(0.5**p))
+		for j in range(0,len(M4)):
+			p = len(nx.shortest_path(G,M1[i],M4[j]))
+			if p<=Max_Hop:
+				sum4 = sum4 + Max_credit*(1-(0.5**p))
+		print (sum1/(len(M1)-1) , sum2/len(M2) , sum3/len(M3) , sum4/len(M4))
+	print ("\n")
+
+
 G = nx.Graph()
 
 nodes = []
@@ -30,11 +61,77 @@ for i in nodes:
 		G.node[i]['car'] = 0
 
 
+
+M1 = []
+M2 = []
+M3 = []
+M4 = []
+S1 = []
+S2 = []
+S3 = []
+S4 = []
+C1 = []
+C2 = []
+C3 = []
 mobile=nx.get_node_attributes(G,'mobile')
-print(mobile)
+shoes=nx.get_node_attributes(G,'shoes')
+car=nx.get_node_attributes(G,'car')
+for i in mobile:
+	if (mobile[i] == 1):
+		M1.append(i)
+	elif (mobile[i] == 2):
+		M2.append(i)
+	elif (mobile[i] == 3):
+		M3.append(i)
+	elif (mobile[i] == 4):
+		M4.append(i)
 
 
-nx.draw(G,with_labels = True)
+for i in shoes:
+	if (shoes[i] == 1):
+		S1.append(i)
+	elif (shoes[i] == 2):
+		S2.append(i)
+	elif (shoes[i] == 3):
+		S3.append(i)
+	elif (shoes[i] == 4):
+		S4.append(i)
+
+for i in car:
+	if (car[i] == 1):
+		C1.append(i)
+	elif (car[i] == 2):
+		C2.append(i)
+	elif(car[i] == 3):
+		C3.append(i)
+
+"""
+Caluculte_avg_distance_mobile(M1,M2,M3,M4)
+Caluculte_avg_distance_mobile(M2,M1,M3,M4)
+Caluculte_avg_distance_mobile(M3,M1,M3,M4)
+Caluculte_avg_distance_mobile(M4,M1,M3,M4)
+
+Caluculte_avg_distance_mobile(S1,S2,S3,S4)
+Caluculte_avg_distance_mobile(S2,S1,S3,S4)
+Caluculte_avg_distance_mobile(S3,S1,S3,S4)
+Caluculte_avg_distance_mobile(S4,S1,S3,S4)
+"""
+
+
+color_map = []
+for node in G:
+    if node in C1:
+        color_map.append('blue')
+    elif node in C2:
+    	color_map.append('red')
+    elif node in C3:
+    	color_map.append('orange')
+    else:
+    	color_map.append('black')   
+
+
+
+nx.draw(G,node_color = color_map,with_labels = True)
 plt.show()
 
 
